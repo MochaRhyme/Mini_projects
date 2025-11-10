be2e5af686a7dce0bea6d399962aa56eb07aa0db
MochaRhyme <tvasuper@gmail.com>
2025-11-10 10:19:07 +0900

    Automate commiting...

--- c/array.c
+++ c/array.c
@@ -1,16 +1,23 @@
 #include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
 #include<math.h>
-void swapInt(int* a,int* b){
+void swapIntVar(int* a,int* b){
     int temp=*a;
     *a=*b;
     *b=temp;
 }
 void reverseIntArray(int* arr,int len){
     for(int i=0;i<len/2;i++){
-        swapInt(&arr[i],&arr[len-1-i]);
+        swapIntVar(&arr[i],&arr[len-1-i]);
     }
 }
-
+int collectRand(int start,int end){
+    return rand()%(end-start+1)+start;
+}
 int main(){
-    
+    srand((unsigned int)time(NULL));
+    for(int i=0;i<50;i++){
+        printf("%d\n",collectRand(2,4));
+    }
 }
