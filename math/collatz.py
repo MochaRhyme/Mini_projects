336a74b61e4664a384b213dccda413dec3ec7bf3
MochaRhyme <tvasuper@gmail.com>
2025-11-23 15:47:10 +0900

    Add loopable cc, Fix hard coding

--- math/collatz.py
+++ math/collatz.py
@@ -6,76 +6,81 @@ except:
     view=0
 while True:
     try:
-        n=int(input('input intager:'))
+        n=int(input('input natural number:'))
+        if n<0:
+            raise
         break
     except:
-        print('Please input intager!')
+        print('Please input natural number')
 
-nT=n
-Tstep=0
-resT=[n]
-print('---T(n) : General Collatz Conjecture---')
-while nT!=1:
-    #콜라츠 추측의 일반적 표현인 T(n)값 계산
-    if nT%2==0:
-        print(f'{nT} is even :',end=' ')
-        nT=nT//2
-        resT.append(nT)
-    else:
-        print(f'{nT} is odd :',end=' ')
-        nT=3*nT+1
-        resT.append(nT)
-    Tstep+=1
-    print(f'changed {nT} in {Tstep} step')
-print(resT)
+steps=[0,0,0]
 
-nTP=n
-TPstep=0
-resTP=[n]
-print('---T\'(n) : Step optimization of Collatz Conjecture---')
-while nTP!=1:
-    if nTP%2==0:
-        print(f'{nTP} is even :',end=' ')
-        nTP=nTP//2
-        resTP.append(nTP)
-    else:
-        print(f'{nTP} is odd :',end=' ')
-        nTP=(3*nTP+1)//2
-        resTP.append(nTP)
-    TPstep+=1
-    print(f'changed {nTP} in {TPstep} step')
-print(resTP)
+def cc(l:list,m:str):
+    while l[-1]!=1:
+        if l[-1]%2==0:
+            print(f'{l[-1]} is even :',end=' ')
+            l.append(l[-1]//2)
+        else:
+            print(f'{l[-1]} is odd :',end=' ')
+            if m=='d':
+                l.append(3*l[-1]+1)
+            elif m=='p':
+                l.append((3*l[-1]+1)//2)
+            elif m=='pp':
+                if 3*l[-1]-1 in l:
+                    print(f'[loop detected in {steps[2]+1} step]')
+                    break
+                l.append(3*l[-1]-1)
+        print(f'changed {l[-1]} in',end=' ')
+        if m=='d':
+            steps[0]+=1
+            print(steps[0],end=' ')
+        elif m=='p':
+            steps[1]+=1
+            print(steps[1],end=' ')
+        elif m=='pp':
+            steps[2]+=1
+            print(steps[2],end=' ')
+        print('step')
 
-print('\nmax from T(n) :',max(resT[1:]))
-
-if view:
-    x=range(Tstep+1)
-    plt.plot(resT,color='blue',marker='o',label='T(n)')
-    plt.plot(resTP,color='orange',marker='o',linestyle='--',label='T\'(n)')
-    # plt.xticks(x)
-    #this code by gemini(copy from code below)---
-    for i, value in enumerate(resTP):
+def vcc(l:list,color:str,linestyle:str,label:str,):
+    plt.plot(l,color=color,marker='o',linestyle=linestyle,label=label)
+    #this code by gemini---
+    for i, value in enumerate(l):
         plt.annotate(
             str(value),
-            (x[i], resTP[i]),
-            color='orange',
+            (x[i], l[i]),
+            color=color,
             xytext=(0, -13),
             textcoords='offset points',
             ha='center',
             fontsize=8
         )
     #---
-    #this code by gemini---
-    for i, value in enumerate(resT):
-        plt.annotate(
-            str(value),
-            (x[i], resT[i]),
-            xytext=(0, 5),
-            textcoords='offset points',
-            ha='center',
-            fontsize=8
-        )
-    #---
+
+resT=[n]
+print('\n---T(n) : General Collatz Conjecture---')
+cc(resT,'d')
+print(resT)
+
+resTP=[n]
+print('\n---T\'(n) : Step optimization of Collatz Conjecture---')
+cc(resTP,'p')
+print(resTP)
+
+resTPP=[n]
+print('\n---T\'\'(n) : Collatz Conjecture that can be looped---')
+cc(resTPP,'pp')
+print(resTPP)
+
+print('\nmax from T(n) :',max(resT[1:]))
+
+if view:
+    x=range(max(steps)+1)
+    plt.xticks(x)
+    vcc(resTPP,'red','dotted','T\'\'(n)')
+    vcc(resTP,'orange','--','T\'(n)')
+    vcc(resT,'blue','solid','T(n)')
     plt.legend()
     plt.title(f'Collatz Conjecture at {n}')
     plt.xlabel('step')
