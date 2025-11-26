bceba2db856a69355318ac8119a07a4a42299e85
MochaRhyme <tvasuper@gmail.com>
2025-11-26 15:23:30 +0900

    한국어로 작성 및 cui에 색상을 입혀봄
    파이썬이 c언어처럼 사용자 환경 인코딩을 따라가는 줄 알았다.
    근데 utf-8이 고정이었다.
    왜 영어로 했지?
    그래서 이걸 한국어로 바꿨다. 설명끝

--- math/collatz.py
+++ math/collatz.py
@@ -1,47 +1,37 @@
-try:
-    import matplotlib.pyplot as plt
-    view=1
-except:
-    print('matplotlib is not installed : visualization code will not work!')
-    view=0
-while True:
-    try:
-        n=int(input('input natural number:'))
-        if n<0:
-            raise
-        break
-    except:
-        print('Please input natural number')
+def AE(string:object='',code:str='0'):
+    if not isinstance(string,str):
+        string=str(string)
+    return '\x1b['+code+'m'+string+'\x1b[0m'
 
 steps=[0,0,0]
 
 def cc(l:list,m:str):
     while l[-1]!=1:
         if l[-1]%2==0:
-            print(f'{l[-1]} is even :',end=' ')
+            print(AE(f'{l[-1]}은/는 짝수','93')+'입니다. :',end=' ')
             l.append(l[-1]//2)
         else:
-            print(f'{l[-1]} is odd :',end=' ')
+            print(AE(f'{l[-1]}은/는 홀수','93')+'입니다. :',end=' ')
             if m=='d':
                 l.append(3*l[-1]+1)
             elif m=='p':
                 l.append((3*l[-1]+1)//2)
             elif m=='pp':
                 if 3*l[-1]-1 in l:
-                    print(f'[loop detected in {steps[2]+1} step]')
+                    print(AE(f'[{steps[2]+1} 번째에 루프가 감지되어 종료합니다.]','91'))
                     break
                 l.append(3*l[-1]-1)
-        print(f'changed {l[-1]} in',end=' ')
         if m=='d':
             steps[0]+=1
-            print(steps[0],end=' ')
+            print(AE(steps[0],'93'),end=' ')
         elif m=='p':
             steps[1]+=1
-            print(steps[1],end=' ')
+            print(AE(steps[1],'93'),end=' ')
         elif m=='pp':
             steps[2]+=1
-            print(steps[2],end=' ')
-        print('step')
+            print(AE(steps[2],'93'),end=' ')
+        print(AE('번째에 ','93'),end='')
+        print(AE(f'{l[-1]}(으)로 바뀝니다.','93'))
 
 def vcc(l:list,color:str,linestyle:str,label:str,):
     plt.plot(l,color=color,marker='o',linestyle=linestyle,label=label)
@@ -58,22 +48,39 @@ def vcc(l:list,color:str,linestyle:str,label:str,):
         )
     #---
 
+try:
+    import matplotlib.pyplot as plt
+    view=1
+except:
+    print(AE('matplotlib이 설치되어 있지 않음 : 시각화 코드는 동작하지 않습니다!','93'))
+    view=0
+print(f'{AE('<콜라츠 추측>','93')}을 시뮬레이션합니다...')
+while True:
+    try:
+        n=int(input('콜라츠 추측의 최초 값이 되는 자연수를 입력하세요:'))
+        if n<0:
+            raise
+        break
+    except:
+        print(AE('입력한 것이 자연수가 아닙니다!','91'))
+
+
 resT=[n]
-print('\n---T(n) : General Collatz Conjecture---')
+print('\n---T(n) : 일반적인 콜라츠 추측---')
 cc(resT,'d')
 print(resT)
 
 resTP=[n]
-print('\n---T\'(n) : Step optimization of Collatz Conjecture---')
+print('\n---T\'(n) : 실행 횟수를 고려한 콜라츠 추측---')
 cc(resTP,'p')
 print(resTP)
 
 resTPP=[n]
-print('\n---T\'\'(n) : Collatz Conjecture that can be looped---')
+print('\n---T\'\'(n) : 루프될 수도 있는 콜라츠 추측(3n-1)---')
 cc(resTPP,'pp')
 print(resTPP)
 
-print('\nmax from T(n) :',max(resT[1:]))
+print('\nT(n)에서의 최대값 :',max(resT[1:]))
 
 if view:
     x=range(max(steps)+1)
